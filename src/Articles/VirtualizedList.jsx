daa57602be0de79e977c86b445d37326c9141f06
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2019-12-15 11:18:39 +0300

    Use absolute positioning for virtualized list

--- src/Articles/VirtualizedList.jsx
+++ src/Articles/VirtualizedList.jsx
@@ -1,40 +1,37 @@
 import React, {useMemo, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
+import './VirtualizedList.scss';
+
+const VirtualizedListItem = ({children, top, height}) => (
+  <div className="virtualized-list__item" style={{top, height}}>{children}</div>
+)
 
 const VirtualizedList = ({items, className, renderItem, visibleItemsCount, itemHeight}) => {
-  const container = useRef();
 
-  const [itemsToSkip, setItemsToSkip] = useState(0);
+  const [start, setStart] = useState(0);
 
-  const topSpace = useMemo(
-    () => itemsToSkip * itemHeight,
-    [itemsToSkip, itemHeight]
-  );
-
-  const bottomSpace = useMemo(
-    () => itemHeight * Math.max(items.length - visibleItemsCount - itemsToSkip - 2, 0),
-    [items.length, visibleItemsCount, itemHeight, itemsToSkip]
-  );
-
-  const dataSpace = useMemo(
-    () => itemHeight * Math.min(items.length, visibleItemsCount + 2),
-    [itemHeight, items.length, visibleItemsCount]
-  );
+  const end = useMemo(
+    () => Math.min(start + visibleItemsCount, items.length),
+    [start, items.length, visibleItemsCount]
+  )
 
   function onScroll(evt) {
-    const offset = evt.target.scrollTop;
-    const newItemsToSkip = Math.floor(offset / itemHeight);
-    console.log('Offset: ', offset, ', items to skip: ', newItemsToSkip);
-    setItemsToSkip(newItemsToSkip);
+    const offset = Math.floor(evt.target.scrollTop / itemHeight);
+    const newStart = offset + visibleItemsCount > items.length ? items.length - visibleItemsCount : offset
+    requestAnimationFrame(() => setStart(newStart));
   }
 
-  console.log({itemsToSkip, topSpace, bottomSpace, dataSpace});
-
   return (
-    <div className={className} style={{height: dataSpace}} ref={container} onScroll={onScroll}>
-      {<div key="top-space" style={{height: topSpace}}></div>}
-      {items.slice(itemsToSkip, itemsToSkip + visibleItemsCount + 2).map((item, index) => <div key={index}>{renderItem(item)}</div>)}
-      {<div key="bottom-space" style={{height: bottomSpace}}></div>}
+    <div className="virtualized-list" onScroll={onScroll} style={{height: visibleItemsCount * itemHeight}}>
+      <div className="virtualized-list__container" style={{height: items.length * itemHeight}}>
+      {items.slice(start, end).map((item, index) =>
+        <VirtualizedListItem
+          key={index}
+          top={(start + index) * itemHeight}>
+            {renderItem(item)}
+          </VirtualizedListItem>
+      )}
+      </div>
     </div>
   )
 }
