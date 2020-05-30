5bc328b51ca9852cb26305fc2a7e60a48843abd3
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2020-05-30 15:13:04 +0300

    Enable hot module replacement

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Host from './app/Host';
 
+if (module.hot) {
+    module.hot.accept();
+}
+
 ReactDOM.render(<Host />, document.getElementById('root'));
