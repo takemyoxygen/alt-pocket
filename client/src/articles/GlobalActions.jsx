c61ca0a048d9337ef6522494871c00380c874f51
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2020-07-11 10:22:38 +0300

    Add react bootstrap; add navbar

--- client/src/articles/GlobalActions.jsx
+++ client/src/articles/GlobalActions.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import {TiPencil} from 'react-icons/ti';
-import {MdCloudDownload, MdRefresh} from 'react-icons/md';
-import {IoIosLogOut} from 'react-icons/io';
+import {FaPencilAlt, FaSignOutAlt, FaSyncAlt, FaCloudDownloadAlt} from 'react-icons/fa';
 import {connect} from 'react-redux';
 import actions from '../actions';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import './GlobalActions.scss';
 
 const GlobalActions = ({bulkEditEnabled, onBulkToggled, sync, reloadAll, logout}) => (
-  <div className="global-operations">
-  <TiPencil
-    title="Bulk edit"
-    className={bulkEditEnabled ? 'bulk-edit-icon--enabled' : ''}
-    onClick={onBulkToggled}
-  />
-  <MdCloudDownload title="Sync" onClick={sync}/>
-  <MdRefresh title="Reload all" onClick={reloadAll} />
-  <IoIosLogOut title="Log out" onClick={logout}/>
-</div>
+  <Navbar bg="light" variant="light" className="header">
+    <Navbar.Brand className="mr-auto">Alt. Pocket</Navbar.Brand>
+    <Nav>
+      <NavDropdown title="Actions" id="basic-nav-dropdown">
+        <NavDropdown.Item onClick={onBulkToggled} className="header__action">
+          <FaPencilAlt/>
+          {bulkEditEnabled ? 'Cancel bulk edit' : 'Bulk edit'}
+          </NavDropdown.Item>
+        <NavDropdown.Item onClick={sync} className="header__action">
+          <FaSyncAlt/>
+          Sync
+        </NavDropdown.Item>
+        <NavDropdown.Item onClick={reloadAll} className="header__action">
+          <FaCloudDownloadAlt/>
+          Reload all
+        </NavDropdown.Item>
+        <NavDropdown.Item onClick={logout} className="header__action">
+          <FaSignOutAlt/>
+          Logout
+          </NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+  </Navbar>
 )
 
 export default connect(
